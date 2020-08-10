7d0e52e6d25ee014595904a14a6261bcd2594bcb
Rafael Melendez <rafaelmelendez92@gmail.com>
2020-08-09 21:34:44 -0500

    docs agregado

---
+++ docs/precache-manifest.154ddcfec2083c793be88bb407deb858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783489abad3ef4df90a0cd65917c54b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86bedf086ac2bb637d2e",
+    "url": "/static/css/main.33d5e159.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86bedf086ac2bb637d2e",
+    "url": "/static/js/main.bb1e6891.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
